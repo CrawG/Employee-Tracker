20b380f487fb00f576f521c26114596dbc842661
Crawford Smith <crawfordgsmith@gmail.com>
2020-11-30 13:32:36 -0500

    Added db files and basic structure for schema

---
+++ db/Employee_Tracker.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+
+
+CREATE TABLE department (
+
+)
+
+CREATE TABLE employee (
+
+)
+
+CREATE TABLE role (
+
+)
